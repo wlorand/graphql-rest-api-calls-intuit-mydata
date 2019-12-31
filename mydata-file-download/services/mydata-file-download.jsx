50e1f4bf1363e2aaaf948cf70a7e0b38ac092d94
Will Lorand <bill@milemarker3.org>
2019-12-30 20:42:34 -0800

    removed api keys

--- mydata-file-download/services/mydata-file-download.jsx
+++ mydata-file-download/services/mydata-file-download.jsx
@@ -2,56 +2,63 @@ import {
   DOCUMENT_SERVICE_BASE_URL,
   BOX_API_DOWNLOAD_BASE_URL,
   BOX_FIELD_DOWNLOAD_URL
-} from "../../constants";
-import {getHandledServiceRequest} from "../ServiceHandler";
-import {callHandleServiceRequest, getRequestInfo} from "./mydata-work-orders";
-import {downloadZipFiles} from "../../bl/mydata/mydata-file-download";
+} from '../../constants';
+import { getHandledServiceRequest } from '../ServiceHandler';
+import { callHandleServiceRequest, getRequestInfo } from './mydata-work-orders';
+import { downloadZipFiles } from '../../bl/mydata/mydata-file-download';
 
 /**
-* Call the Box API to GET full download links
-* @param {Array} documentIds: box documentIds that resolve to zip files
-* @param {String} accessTokenString: box api acces token
-*/
+ * Call the Box API to GET full download links
+ * @param {Array} documentIds: box documentIds that resolve to zip files
+ * @param {String} accessTokenString: box api acces token
+ */
 export function getBoxFilesDownloadLinks(documentIds, accessTokenString) {
-    const boxUrls = documentIds.map(docId => `${BOX_API_DOWNLOAD_BASE_URL}${docId}?fields=${BOX_FIELD_DOWNLOAD_URL}`);
-    const boxHeaders = {
-        "Authorization": `Bearer ${accessTokenString}`,
-        "Content-Type": "application/zip",
-        "Content-Description": "File Transfer",
-        "Accept-Encoding": "gzip, compress, deflate"
-    };
-    const pluginName = "iam-account-manager-ui";
-    const serviceName = "mydata-get-box-files-download-links";
-    const promisesArray = [];
-    boxUrls.map(boxUrl => {
-        promisesArray.push(getHandledServiceRequest(
-            {
-                pluginName,
-                serviceName
-            }, (resolve, reject) => {
-            fetch(boxUrl, {
-                headers: boxHeaders
+  const boxUrls = documentIds.map(
+    (docId) =>
+      `${BOX_API_DOWNLOAD_BASE_URL}${docId}?fields=${BOX_FIELD_DOWNLOAD_URL}`
+  );
+  const boxHeaders = {
+    Authorization: `Bearer ${accessTokenString}`,
+    'Content-Type': 'application/zip',
+    'Content-Description': 'File Transfer',
+    'Accept-Encoding': 'gzip, compress, deflate'
+  };
+  const pluginName = 'iam-account-manager-ui';
+  const serviceName = 'mydata-get-box-files-download-links';
+  const promisesArray = [];
+  boxUrls.map((boxUrl) => {
+    promisesArray.push(
+      getHandledServiceRequest(
+        {
+          pluginName,
+          serviceName
+        },
+        (resolve, reject) => {
+          fetch(boxUrl, {
+            headers: boxHeaders
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              resolve(data.download_url);
             })
-            .then(response => response.json())
-            .then(data => {
-                resolve(data.download_url);
-            })
-            .catch(err => reject(err));
-        }));
-    });
+            .catch((err) => reject(err));
+        }
+      )
+    );
+  });
 
-    Promise.all(promisesArray).then(response => {
-        const boxcloudUrls = [];
-        response.forEach(boxcloudUrl => {
-            boxcloudUrls.push(Promise.resolve(boxcloudUrl.response));
-        });
-        // 3- perform the actual download
-        downloadZipFiles(boxcloudUrls);
+  Promise.all(promisesArray).then((response) => {
+    const boxcloudUrls = [];
+    response.forEach((boxcloudUrl) => {
+      boxcloudUrls.push(Promise.resolve(boxcloudUrl.response));
     });
-};
+    // 3- perform the actual download
+    downloadZipFiles(boxcloudUrls);
+  });
+}
 
-export function getDownloadAccessToken(documentId, env = "e2e") {
-    const query = `
+export function getDownloadAccessToken(documentId, env = 'e2e') {
+  const query = `
     {
       "docSpace": "dg2",
       "targetEntities": {
@@ -64,6 +71,12 @@ export function getDownloadAccessToken(documentId, env = "e2e") {
     ]
   }
   `;
-    const requestInfo = getRequestInfo(env, DOCUMENT_SERVICE_BASE_URL, "preprdakyresMiM27Q3l12Fcg81vHuH8fGm7d7Z5", "prdakyresh3dP1M4JldjgUssgx6YdZHSx5UJxb4c");
-    return callHandleServiceRequest("iam-account-manager-ui", "mydata-fetch-download-access-token", requestInfo, query, "POST");
+  const requestInfo = getRequestInfo(env, DOCUMENT_SERVICE_BASE_URL, '', '');
+  return callHandleServiceRequest(
+    'iam-account-manager-ui',
+    'mydata-fetch-download-access-token',
+    requestInfo,
+    query,
+    'POST'
+  );
 }
